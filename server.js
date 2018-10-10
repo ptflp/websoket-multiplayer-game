09ba6c9d30f2aa1326d6ed92fc1bb9e80de2d419
Petr Filippov <filippov.pa@s-vfu.ru>
2018-10-10 09:17:02 +0000

    port change

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
- 
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const PORT = process.env.PORT || 5000
 app.use(express.static(__dirname + '/public'));
  
 app.get('/', function (req, res) {
