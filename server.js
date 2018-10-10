e453eb617c968d96a76ba25aa46ff68022694d7c
Petr Filippov <filippov.pa@s-vfu.ru>
2018-10-10 09:55:00 +0000

    add socket.io

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const io = require('socket.io').listen(server);
 const PORT = process.env.PORT || 5000
 app.use(express.static(__dirname + '/public'));
  
@@ -10,4 +11,11 @@ app.get('/', function (req, res) {
  
 server.listen(PORT, function () {
   console.log(`Listening on ${server.address().port}`);
+});
+
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
 });
