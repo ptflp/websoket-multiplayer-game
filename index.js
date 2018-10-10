521c7dd81f43a93dd59326ef282e4bdb3e53dd64
Petr Filippov <filippov.pa@s-vfu.ru>
2018-10-10 09:04:22 +0000

    Add Database

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const cool = require('cool-ascii-faces')
 const express = require('express')
-const path = require('path')
+const path = require('path')const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 const PORT = process.env.PORT || 5000
 
 express()
@@ -10,11 +14,23 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .get('/times', (req, res) => {
-  let result = ''
-  const times = process.env.TIMES || 5
-  for (i = 0; i < times; i++) {
-    result += i + ' '
-  }
-  res.send(result)
-})
+	  let result = ''
+	  const times = process.env.TIMES || 5
+	  for (i = 0; i < times; i++) {
+	    result += i + ' '
+	  }
+	  res.send(result)
+	})
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
